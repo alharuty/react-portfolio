9e47285732f28422af49208932b91125999a6f3c
allharut <alla-pdg@hotmail.es>
2024-07-05 11:23:01 +0200

    commit1

--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import moment from "moment";
 
+import PortfolioContainer from './portfolio-container';
+
 export default class App extends Component {
-  render() {
-    return (
-      <div className='app'>
-        <h1>React Portfolio by @allharuty</h1>
-        <h2>For DevCamp FullStack Bootcamp</h2>
-        <div class="my_date">
-          <h3>This portfolio has been created:</h3>{moment("20240702", "YYYYMMDD").fromNow()}
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return ( <
+            div className = 'app' >
+            <h1> React Portfolio by @allharuty </h1>
+            <h2> For DevCamp FullStack Bootcamp </h2>
+            <div class="my_date">
+            <h3> This portfolio has been created: </h3>{moment("20240702", "YYYYMMDD").fromNow()}
+            </div>
+            <PortfolioContainer />
+            </div>
+        );
+    }
 }
