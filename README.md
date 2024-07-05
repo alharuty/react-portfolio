23085ab8836145f7181431cabc742d7f880a6b8c
allharut <alla-pdg@hotmail.es>
2024-07-05 10:03:36 +0200

    React App first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# REACT portfolio Application by Alla Harutyunyan
+
---
+++ src/components/app.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import moment from "moment";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>React Portfolio by @allharuty</h1>
+        <h2>For DevCamp FullStack Bootcamp</h2>
+        <div class="my_date">
+          <h3>This portfolio has been created:</h3>{moment("20240702", "YYYYMMDD").fromNow()}
+        </div>
+      </div>
+    );
+  }
+}
